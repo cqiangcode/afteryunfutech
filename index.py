2bfa8b2c1ec5f1bd881b3cb1725e821574f6aae8
chenqiang <32243594@qq.com>
2019-05-07 08:39:23 +0800

    5/7

--- index.py
+++ index.py
@@ -1,22 +1,11 @@
-# from pyspark.sql import SparkSession
-
-# spark = SparkSession.builder.appName('electric').getOrCreate()
-
-# csv_df = spark.read.csv('../data/2015/Apt7_2015.csv')
-
-# df = csv_df.toPandas()
-
-# print(df[:, 1])
-
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-df = pd.read_csv('./Apt1_2015.csv')
+df = pd.read_csv('./Apt1_2016.csv', header=None)
 front = 0
 cycle = 0
 position = 0
 power = []
-print(len(df.values))
 for value in df.values:
     if int(value[0][14: 16]) % 15 == 0:
         cycle = cycle + value[1]
@@ -28,9 +17,8 @@ for value in df.values:
         cycle = 0
     else:
         cycle = cycle + value[1]
-print(len(power))
 del power[len(power) - 1]
 # df = pd.DataFrame(power)
-estimator = KMeans(n_clusters=2)#构造聚类器
+estimator = KMeans(n_clusters=5)#构造聚类器
 estimator.fit(power)
-print(type(estimator.labels_))
+print(estimator.labels_)
